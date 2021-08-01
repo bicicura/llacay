352afb75f5134381ba8f563cd4338553c24257f4
Franco La Mattina <fr4ncolamattina@gmail.com>
2021-08-01 18:24:29 -0300

    Avances 1/8
    adaptacion a mobile y footer barra de comentarios en recos

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,3 +1,13 @@
+//  IMPORTANTE
+//  Detecta si el browser es mobile, sirve para asignar la altura correcta al contendor en dispositivos mobiles en recos. 
+//  Tmb para asignar display none en mobile a las transiciones que no se usan, asi se evita q el user pueda scrollear a las transiciones
+
+window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+    };
+
 // Nota: Todo el contenido dinamico de la web se pone inserta en el elemento de html MAIN
 
 
@@ -102,11 +112,15 @@ links.forEach( link => {
 
         menuRestart()
 
+        if (window.mobileCheck()) {
+            mobileIndexHideTransitions()
+        }
         // Cheque en que vista está, para hacer las transiciones y traer el js que necesita
         checkInspiracion()
         checkGaleria()
         checkContacto()
         checkRecos()
+        
     }) 
 
     
@@ -150,6 +164,9 @@ window.addEventListener("popstate", ev => {
     const url = history.state+'.html'
 
     // Cheque en que vista está, para hacer las transiciones y traer el js que necesita
+    if (window.mobileCheck()) {
+        mobileIndexHideTransitions()
+    }
     checkContacto()
     checkInspiracion()
     checkGaleria()
@@ -243,6 +260,20 @@ function cortinaFadeIn() {
 function cortinaFadeOut() {
     cortinaTransition.classList.remove('fade-in')
     cortinaTransition.classList.add('fade-out')
+}
+
+function addDisplayNoneTransitions() {
+    // bgMaskTransition.classList.add('display-none')
+    inspiracionBg.classList.add('display-none')
+    recosBg.classList.add('display-none')
+    // contactoBg.classList.add('display-none')
+}
+
+function removeDisplayNoneTransitions() {
+    // bgMaskTransition.classList.remove('display-none')
+    inspiracionBg.classList.remove('display-none')
+    recosBg.classList.remove('display-none')
+    // contactoBg.classList.remove('display-none')
 }
 
 
@@ -396,14 +427,23 @@ function recosBgTransitionReset() {
 // ======================================================================================
 
 function inspiracionAnimScroll() {
-
     // Hace que el texto y la layer opaca se vayan y no se vuelva a repetir ese evento
-    $(document).one('scroll', ev => {
-        const inspiracionMainText = document.querySelector('.inspiracion-mainText__container')
-        console.log('escroleaste!')
-        inspiracionMainText.classList.add('fade-out')
-        bgMaskTransition.classList.add('fade-out-scroll')        
-    })
+    if (!window.mobileCheck()) {
+            $(document).one('scroll', ev => {
+                const inspiracionMainText = document.querySelector('.inspiracion-mainText__container')
+                console.log('escroleaste!')
+                inspiracionMainText.classList.add('fade-out')
+                bgMaskTransition.classList.add('fade-out-scroll')
+            })
+        }
+        else {
+            $(window).one('touchend', function() {
+                const inspiracionMainText = document.querySelector('.inspiracion-mainText__container')
+                console.log('escroleaste en mobile!')
+                inspiracionMainText.classList.add('fade-out')
+                bgMaskTransition.classList.add('fade-out-scroll')
+            })
+        }
 }
 
 
@@ -440,7 +480,16 @@ function galeriaColorEnViewport() {
 
 }
 
+// ============================================================
+// MENU MOBILE se cierra al apretar un li y se le asigna active
+// ============================================================
 
+$('.nav-a').click(function(ev) {
+    if (!window.mobileCheck()) return
+    else {
+        $('#menuToggle input').trigger('click')
+    }
+})
 
 // =======================================
 // Transición de cualquier vista a GALERIA 
@@ -479,12 +528,76 @@ function galeriaBgTransitionReset() {
     bgMaskTransition.classList.add('fade-out')
 }
 
+
+// ACA ARRANCA PARA HACER TAPAR EN MOBILE EN INDEX Y CONTACTO LAS TRANSICIONES DE ABAJO
+
+function mobileIndexHideTransitions() {
+    if (!window.mobileCheck) return
+    else{
+        if (!window.location.pathname.includes("inspiracion-spa") &&
+        !window.location.pathname.includes("contacto-spa") &&
+        !window.location.pathname.includes("gallery-spa") &&
+        !window.location.pathname.includes("recomendaciones")
+        ) {
+                bgMaskTransition.classList.add('display-none')
+                inspiracionBg.classList.add('display-none')
+                recosBg.classList.add('display-none')
+                contactoBg.classList.add('display-none')
+            } else {
+                bgMaskTransition.classList.remove('display-none')
+                inspiracionBg.classList.remove('display-none')
+                recosBg.classList.remove('display-none')
+                contactoBg.classList.remove('display-none')
+            } 
+        }
+    }
+
+
+mobileIndexHideTransitions()
+
+
+// ===================================================
+// Para que se fadea el footer correcto segun la vista
+// ===================================================
+
+function bottomBarFade(){
+    $(".bottom-lines__dashed-recos").addClass('fade-out')
+    $(".bottom-lines__active").removeClass('fade-out')
+    $(".bottom-lines__active").addClass('fade-in')
+    $(".bottom-lines__left, .bottom-lines__right").removeClass('fade-out')
+    $(".bottom-lines__left, .bottom-lines__right").addClass('fade-in')
+}
+
+function recosBottomBarFade() {
+
+    setTimeout(() => {
+        $(".bottom-lines__left, .bottom-lines__right").removeClass('fade-in')
+        $(".bottom-lines__left, .bottom-lines__right").addClass('fade-out')
+        $(".bottom-lines__active").removeClass('fade-in')
+        $(".bottom-lines__active").addClass('fade-out')
+        $(".bottom-lines__dashed-recos").removeClass('fade-out')
+        $(".bottom-lines__dashed-recos").addClass('fade-in')
+    
+    }, 1000);  
+
+
+}
+
+
 // ===================================================================
 // Para que carguen los JS de animaciones cuando se actualiza la vista
 // ===================================================================
 
-function checkGaleria() {
+    function checkGaleria() {
     if (window.location.pathname.includes("gallery-spa")) {
+        if (window.mobileCheck()) {
+            removeDisplayNoneTransitions()
+            bgMaskTransition.classList.add('display-none')
+                inspiracionBg.classList.add('display-none')
+                recosBg.classList.add('display-none')
+                contactoBg.classList.add('display-none')
+        }
+        bottomBarFade()
         removeDisappear()
         cortinaFadeIn()
         galeriaTransition()
@@ -514,6 +627,10 @@ function checkGaleria() {
 
 function checkInspiracion() {
     if (window.location.pathname.includes("inspiracion-spa")) {
+        if (window.mobileCheck()) {
+            removeDisplayNoneTransitions()
+        }
+        bottomBarFade()
         removeDisappear()
         contactoBgTransitionReset()
         recosBgTransitionReset()
@@ -523,22 +640,42 @@ function checkInspiracion() {
 }
 
 function checkRecos() {
-    
     if (window.location.pathname.includes("recomendaciones")) {
+        if (window.mobileCheck()) {
+            removeDisplayNoneTransitions()
+        }
+        $('.bottom-lines__active').css({'transform':'translateX(300%)'})
+        recosBottomBarFade()
         removeDisappear()
         contactoBgTransitionReset()
         inspiracionBgTransitionReset()
         recosBgTransition()
         setTimeout(() => {
-            recomendacionesLogica()
+            waitUntil(
+                function () {
+                  // the code you want to run here...
+                  recomendacionesLogica()
+                },
+                function() {
+                  // the code that tests here... (return true if test passes; false otherwise)
+                  return !!(document.getElementById('texts_holder').innerHTML !== '');
+                },
+                50 // amout to wait between checks
+              )();            
             }, 1500);
-            $('.bottom-lines__active').css({'transform':'translateX(300%)'})
         }
        
     }
 
+
+
 function checkContacto() {
     if (window.location.pathname.includes("contacto-spa")) {
+        if (window.mobileCheck()) {
+            removeDisplayNoneTransitions()
+        }
+        bottomBarFade()
+        $(".bottom-lines__dashed-recos").addClass('fade-out')
         // window.scrollTo(0, 0)
         removeDisappear()
         recosBgTransitionReset()
@@ -547,6 +684,9 @@ function checkContacto() {
         setTimeout(() => {
             $(".contact-main-container").ready(function() {
                 inputsDinamicos()
+                if(window.mobileCheck()) {
+                    addDisplayNoneTransitions()
+                }
             })
         }, 2000);     
             
@@ -561,7 +701,7 @@ function checkContacto() {
 
 function inputsDinamicos() {
     $('.inputes').focusin(function(){
-        $("label[for="+$(this).attr("id")+"]").css({'transform':'scale(1) translate(0)','color':'white'})
+        $("label[for="+$(this).attr("id")+"]").css({'transform':'scale(1) translate(0, 9px)','color':'white'})
     })
 
     $('.inputes').focusout(function(){
@@ -643,16 +783,6 @@ function recomendacionesLogica() {
     }
     
 
-//  ===========================================================================================================
-//  Detecta si el browser es mobile, sirve para asignar la altura correcta al contendor en dispositivos mobiles
-//  ===========================================================================================================
-
-    window.mobileCheck = function() {
-        let check = false;
-        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;
-      };
-
 
 //  ==============================================
 //  Acá arrancan la declaracion de cada comentario
@@ -665,10 +795,14 @@ function recomendacionesLogica() {
 
         if (display1) return
         else {
+            $('.bottom-lines__dashed_3').removeClass('fade-in')
+            $('.bottom-lines__dashed_2').removeClass('fade-in')
+            $('.bottom-lines__dashed_1').addClass('fade-in')
             if (window.mobileCheck()) {
                 el.style.height = '30.5rem'
                 } 
             else {
+                
                 el.style.height = '21.875rem'
                 }
         return [el2.innerHTML = `<div class="reco-texts__1">
@@ -688,6 +822,9 @@ function recomendacionesLogica() {
 
         if (display2) return
         else {
+            $('.bottom-lines__dashed_1').removeClass('fade-in')
+            $('.bottom-lines__dashed_3').removeClass('fade-in')
+            $('.bottom-lines__dashed_2').addClass('fade-in')
             if (window.mobileCheck()) {
                 el.style.height = '16.2rem'
               } 
@@ -709,6 +846,9 @@ function recomendacionesLogica() {
 
         if (display3) return
         else {
+            $('.bottom-lines__dashed_1').removeClass('fade-in')
+            $('.bottom-lines__dashed_2').removeClass('fade-in')
+            $('.bottom-lines__dashed_3').addClass('fade-in')
             if (window.mobileCheck()) {
                 el.style.height = '17.5rem'
               } 
@@ -798,6 +938,7 @@ function recomendacionesLogica() {
             display1 = true
             display2 = false
             display3 = false
+            
             
         };
         
